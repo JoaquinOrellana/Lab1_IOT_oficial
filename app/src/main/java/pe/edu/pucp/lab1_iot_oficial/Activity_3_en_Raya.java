06dcf086c14c5cd424850ce998f7e2e5b1f64de1
Joaquin <joaquin.orellana@pucp.edu.pe>
2022-09-02 08:41:04 -0500

    Activity 3 en raya

--- app/src/main/java/pe/edu/pucp/lab1_iot_oficial/Activity_3_en_Raya.java
+++ app/src/main/java/pe/edu/pucp/lab1_iot_oficial/Activity_3_en_Raya.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_3_en_Raya extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_3_en_raya);
     }
+
 
 
 }
