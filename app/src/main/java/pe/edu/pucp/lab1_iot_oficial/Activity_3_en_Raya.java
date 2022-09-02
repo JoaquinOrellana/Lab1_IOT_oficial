580310d93d07eb50679aa9e1c3f3dec478e9b347
Joaquin <joaquin.orellana@pucp.edu.pe>
2022-09-02 10:01:16 -0500

    Implementacion matriz inicial 3 en raya

--- app/src/main/java/pe/edu/pucp/lab1_iot_oficial/Activity_3_en_Raya.java
+++ app/src/main/java/pe/edu/pucp/lab1_iot_oficial/Activity_3_en_Raya.java
@@ -3,6 +3,9 @@ package pe.edu.pucp.lab1_iot_oficial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Activity_3_en_Raya extends AppCompatActivity {
 
@@ -11,6 +14,36 @@ public class Activity_3_en_Raya extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3_en_raya);
     }
+    int i =0;
+    public void actualizarEstado1erJugadorCasilla1(View view){
+
+        TextView [][] matriz3x3= {{findViewById(R.id.casilla1),findViewById(R.id.casilla2),findViewById(R.id.casilla3)},
+                {findViewById(R.id.casilla4),findViewById(R.id.casilla5),findViewById(R.id.casilla6)},
+                {findViewById(R.id.casilla7),findViewById(R.id.casilla8),findViewById(R.id.casilla9)}};
+
+
+        TextView valorCasilla= (TextView) view;
+        if (valorCasilla.getText().toString().equals("----") & (i)%2==0){
+            valorCasilla.setText("x");
+        }else if(valorCasilla.getText().toString().equals("----")){
+            valorCasilla.setText("o");
+        }
+
+
+        i++;
+        Log.d("msg", String.valueOf(view.getId()));
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 
 
